9d7dc1dcc986b2aaff759dedde894a436c7d1b61
Stasy Bolotnikova <stasybol95@gmail.com>
2025-03-15 13:04:50 +0300

    Constants classes

---
+++ src/main/java/site/nomoreparties/stellarburgers/constant/Browser.java
@@ -0,0 +1,6 @@
+package site.nomoreparties.stellarburgers.constant;
+
+public class Browser {
+    public static final String BROWSER_CHROME = "CHROME";
+    public static final String BROWSER_YANDEX = "YANDEX";
+}
